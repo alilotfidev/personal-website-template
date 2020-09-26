b8dedf3bc5ce033f8ea3c98cf20d6511d65bc53e
alilotfi.dev <lotfi26.ali@gmail.com>
2020-09-26 15:26:14 +0330

    intro is ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import './css/keyframes.css';
+
+//import components
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="App">
-     
+      <div id="content">
+        <Intro />
+      </div>
+      <div id="side-bar"></div>
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../css/Intro.css';
+import PersonImage from '../images/anna-edmunds.jpg';
+import DownArrow from '../images/down-arrow.svg';
+const Intro = () => {
+  return (
+    <div className="Intro">
+        <h1 className="intro-name">Hello<br/> I’m <span>Anna Edmunds</span></h1>
+        <img className="intro-image" src={PersonImage} alt="anna edmunds"/>
+        <img className="arrow" src={DownArrow} alt="down arrow"/>
+    </div>
+  );
+};
+
+export default Intro;
