630c754f774e2d61b6c8153f34e1a3d041ab4a62
alilotfi.dev <lotfi26.ali@gmail.com>
2023-09-27 14:20:01 +0330

    bug fixes

--- components/ui/CursorProvider.js
+++ components/ui/CursorProvider.js
@@ -10,10 +10,13 @@ export default function CursorProvider({ children }) {
     setCursorY(e.pageY);
   };
   return (
-    <div className="cursor-none" onMouseMove={(e) => handleCustomCursor(e)}>
+    <div
+      className="relative cursor-none overflow-x-hidden"
+      onMouseMove={(e) => handleCustomCursor(e)}
+    >
       {children}
       <div
-        className="cursor pointer-events-none absolute z-50 h-8 w-8 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white mix-blend-difference transition-transform duration-300"
+        className="cursor pointer-events-none absolute z-50 h-8 w-8 -translate-x-1/2 -translate-y-1/2 overflow-x-auto rounded-full bg-white mix-blend-difference transition-transform duration-300"
         style={{ top: cursorY, left: cursorX }}
       ></div>
     </div>
