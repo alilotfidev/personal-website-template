2383b6815eeb080a62d29c632a60652ed87eb6d0
alilotfi.dev <lotfi26.ali@gmail.com>
2023-09-30 15:41:12 +0330

    image bug fixed

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -1,13 +1,16 @@
+import Image from "next/image";
+import BackgroundImage from "@/public/background.jpg";
 export default function HeroImage() {
   return (
-    <section className="hero-background-image relative min-h-screen bg-cover bg-center px-10 py-8 text-white">
-      <h2 className="hero-section-text absolute top-1/3 ml-12 -translate-y-1/3 text-4xl font-bold leading-tight md:text-5xl lg:text-6xl">
+    <section className="hero-background-image relative flex min-h-screen items-center justify-between px-10 py-8 text-white">
+      <h2 className="hero-section-text text-3xl font-bold leading-tight md:text-5xl lg:text-6xl">
         Strategic design
         <br />
         for the brands
         <br />
         of tomorrow.
       </h2>
+      <Image src={BackgroundImage} alt="" width={400} height={800} />
     </section>
   );
 }
