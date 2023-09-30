c8f2282b4915fc5b682b7957b632e91163544e70
alilotfi.dev <lotfi26.ali@gmail.com>
2023-09-30 15:19:06 +0330

    contact me is responsive

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -2,12 +2,14 @@ export default function ContactMe() {
   return (
     <section className="flex min-h-screen items-center justify-center bg-neutral-100 px-10 py-8 text-black">
       <div className="start-project-cta">
-        <h4 className="text-center text-5xl font-bold">start your project.</h4>
-        <div className="start-project-cta-buttons my-10 flex items-center justify-between gap-8">
-          <button className="rounded-full border border-black px-8 py-5 transition-all duration-300 hover:bg-black hover:text-white hover:ease-in-out">
+        <h4 className="text-center text-4xl font-bold sm:text-5xl">
+          start your project.
+        </h4>
+        <div className="start-project-cta-buttons my-10 flex flex-col-reverse items-center justify-between gap-8 sm:flex-row">
+          <button className="rounded-full border border-black px-6 py-4 transition-all duration-300 hover:bg-black hover:text-white hover:ease-in-out sm:px-8 sm:py-5">
             process & expertise
           </button>
-          <button className="rounded-full bg-black px-8 py-5 text-white transition-all duration-300 hover:ease-in-out">
+          <button className="rounded-full bg-black px-6 py-4 text-white transition-all duration-300 hover:ease-in-out sm:px-8 sm:py-5">
             book a meeting
           </button>
         </div>
