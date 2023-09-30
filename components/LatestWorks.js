1cd22884951f5a924382f54a4b891f363d79d2a1
alilotfi.dev <lotfi26.ali@gmail.com>
2023-09-30 15:16:00 +0330

    latest works is responsive

--- components/LatestWorks.js
+++ components/LatestWorks.js
@@ -7,10 +7,10 @@ import thirdImage from "@/public/portfolio/3.jpg";
 export default function LatestWorks() {
   return (
     <section className="min-h-screen bg-black px-10 py-8 text-white">
-      <h2 className="latest-works-title text-center text-9xl font-bold">
+      <h2 className="latest-works-title text-center text-5xl font-bold leading-relaxed sm:text-6xl sm:leading-none lg:text-7xl">
         LATEST WORKS
       </h2>
-      <div className="portfolio-item relative mx-auto my-10 w-[50vw] max-w-7xl after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:translate-y-[0.8em] after:bg-white after:opacity-70">
+      <div className="portfolio-item relative mx-auto my-10 w-auto max-w-7xl after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:translate-y-[0.8em] after:bg-white after:opacity-70 sm:w-[50vw]">
         <Image
           src={firstImage}
           alt=""
@@ -18,14 +18,14 @@ export default function LatestWorks() {
           width={800}
           height={500}
         />
-        <div className="portfolio-item-details my-5 flex w-full items-center justify-between text-xs uppercase opacity-90">
+        <div className="portfolio-item-details my-5 flex w-full flex-col items-start justify-between gap-y-4 text-xs uppercase opacity-90 sm:flex-row sm:items-center">
           <div className="portfolio-item-categories">
             brand strategy, corporate identity, 3D/AR
           </div>
           <div className="portfolio-item-brand-type">Tech Start-Up</div>
         </div>
       </div>
-      <div className="home-page-portfolio-grid mx-auto my-10 flex max-w-7xl items-center justify-between gap-x-14">
+      <div className="home-page-portfolio-grid mx-auto my-10 flex max-w-7xl flex-wrap items-center justify-between gap-14">
         <div className="portfolio-item relative mx-auto after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:translate-y-[0.8em] after:bg-white after:opacity-70">
           <Image
             src={secondImage}
@@ -34,7 +34,7 @@ export default function LatestWorks() {
             width={500}
             height={500}
           />
-          <div className="portfolio-item-details mt-5 flex w-full items-center justify-between text-xs uppercase opacity-90">
+          <div className="portfolio-item-details mt-5 flex w-full flex-col items-start justify-between gap-y-4 text-xs uppercase opacity-90 sm:flex-row sm:items-center">
             <div className="portfolio-item-categories">branding & website</div>
             <div className="portfolio-item-brand-type">venture capital</div>
           </div>
@@ -47,7 +47,7 @@ export default function LatestWorks() {
             width={500}
             height={500}
           />
-          <div className="portfolio-item-details mt-5 flex w-full items-center justify-between text-xs uppercase opacity-90">
+          <div className="portfolio-item-details mt-5 flex w-full flex-col items-start justify-between gap-y-4 text-xs uppercase opacity-90 sm:flex-row sm:items-center">
             <div className="portfolio-item-categories">
               Brand Strategy & Corporate Identity
             </div>
