29a5ffb25f7bf5271afb88394fc70ffb19f093c2
alilotfi.dev <lotfi26.ali@gmail.com>
2023-09-30 11:23:29 +0330

    bug fixes

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -1,7 +1,7 @@
 export default function HeroImage() {
   return (
-    <section className="hero-background-image min-h-screen bg-cover bg-center px-10 py-8 text-white">
-      <h2 className="hero-section-text absolute top-2/4 ml-12 -translate-y-2/4 text-6xl font-bold leading-tight">
+    <section className="hero-background-image relative min-h-screen bg-cover bg-center px-10 py-8 text-white">
+      <h2 className="hero-section-text absolute top-1/3 ml-12 -translate-y-1/3 text-6xl font-bold leading-tight">
         Strategic design
         <br />
         for the brands
