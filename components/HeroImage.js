094b1e6b649138e40d79aaa924c9fb6aa6ebd063
alilotfi.dev <lotfi26.ali@gmail.com>
2023-09-30 14:59:47 +0330

    hero text is responsive

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -1,7 +1,7 @@
 export default function HeroImage() {
   return (
     <section className="hero-background-image relative min-h-screen bg-cover bg-center px-10 py-8 text-white">
-      <h2 className="hero-section-text absolute top-1/3 ml-12 -translate-y-1/3 text-6xl font-bold leading-tight">
+      <h2 className="hero-section-text absolute top-1/3 ml-12 -translate-y-1/3 text-4xl font-bold leading-tight md:text-5xl lg:text-6xl">
         Strategic design
         <br />
         for the brands
