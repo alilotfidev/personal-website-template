a016ef879ed353d3917b2024bf642b5c58cd9293
alilotfi.dev <lotfi26.ali@gmail.com>
2023-09-30 15:00:55 +0330

    text information is responsive

--- components/TextInformation.js
+++ components/TextInformation.js
@@ -1,7 +1,7 @@
 export default function TextInformation() {
   return (
     <section className="flex min-h-screen items-center justify-center bg-neutral-100 px-10 py-8 text-black">
-      <div className="intro-text text-4xl leading-normal">
+      <div className="intro-text text-2xl leading-normal md:text-4xl">
         <div className="inner-intro-text bg-white">
           markus is a german designer specialized in strategy-led branding. He
           helps start-ups and medium-sized brands to stand out by combining
