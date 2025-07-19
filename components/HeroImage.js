25490fe2728e3f542d12490d1affe88059b3e315
Ali Lotfi <lotfi26.ali@gmail.com>
2025-07-19 14:04:38 +0330

    updating the project

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -2,15 +2,21 @@ import Image from "next/image";
 import BackgroundImage from "@/public/background.jpg";
 export default function HeroImage() {
   return (
-    <section className="hero-background-image relative flex min-h-screen items-center justify-between px-10 py-8 text-white">
-      <h2 className="hero-section-text text-3xl font-bold leading-tight md:text-5xl lg:text-6xl">
+    <section className="hero-background-image relative flex flex-col items-center justify-between px-10 pt-8 text-white md:min-h-screen md:flex-row">
+      <h2 className="hero-section-text text-center text-3xl font-bold leading-tight md:text-left md:text-5xl lg:text-6xl">
         Strategic design
         <br />
         for the brands
         <br />
         of tomorrow.
       </h2>
-      <Image src={BackgroundImage} alt="" width={400} height={800} />
+      <Image
+        src={BackgroundImage}
+        alt=""
+        width={500}
+        height={800}
+        className="md:self-end"
+      />
     </section>
   );
 }
